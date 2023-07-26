b68271a7a1110d5b15b57139f45c0c067bb55478
Chris Abdo <chris.j.abdo@gmail.com>
2023-07-26 14:45:10 -0400

    ADDED some fixes

--- components/header.tsx
+++ components/header.tsx
@@ -3,17 +3,18 @@ import { motion } from "framer-motion";
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 function SocialLinks() {
   let links = [
     {
       name: "Twitter",
-      href: "/",
+      href: "https://www.x.com/abdo_eth",
       icon: <TwitterLogoIcon className="h-[1.2rem] w-[1.2rem]" />,
     },
     {
       name: "GitHub",
-      href: "/",
+      href: "https://www.github.com/chrisabdo",
       icon: <GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]" />,
     },
   ];
@@ -21,9 +22,16 @@ function SocialLinks() {
   return (
     <>
       {links.map((link) => (
-        <Button key={link.name} variant="ghost" size="icon">
-          {link.icon}
-        </Button>
+        <Link
+          href={link.href}
+          key={link.name}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <Button variant="ghost" size="icon">
+            {link.icon}
+          </Button>
+        </Link>
       ))}
     </>
   );
